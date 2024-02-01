8b50c9d196ee7e817c66a226bdb5ea18c3b2e756
RazinRahat <razinrahat2001@gmail.com>
2024-02-02 01:08:01 +0600

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const sqlite3 = require("sqlite3").verbose();
+const cors = require('cors')
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
